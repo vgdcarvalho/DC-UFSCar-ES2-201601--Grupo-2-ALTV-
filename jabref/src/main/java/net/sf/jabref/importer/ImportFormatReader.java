771ae044809a72b7673ca993a3b119c221b27b3b
Vinicius <vgdcarvalho@gmail.com>
2016-06-20 13:41:49 -0300

    Adicionando CSV à lista de formatos importados

--- jabref/src/main/java/net/sf/jabref/importer/ImportFormatReader.java
+++ jabref/src/main/java/net/sf/jabref/importer/ImportFormatReader.java
@@ -35,6 +35,7 @@ import net.sf.jabref.importer.fileformat.BibTeXMLImporter;
 import net.sf.jabref.importer.fileformat.BiblioscapeImporter;
 import net.sf.jabref.importer.fileformat.BibtexImporter;
 import net.sf.jabref.importer.fileformat.CopacImporter;
+import net.sf.jabref.importer.fileformat.CsvContentImporter;
 import net.sf.jabref.importer.fileformat.EndnoteImporter;
 import net.sf.jabref.importer.fileformat.FreeCiteImporter;
 import net.sf.jabref.importer.fileformat.ImportFormat;
@@ -75,6 +76,7 @@ public class ImportFormatReader {
         formats.add(new BibtexImporter());
         formats.add(new BibTeXMLImporter());
         formats.add(new CopacImporter());
+        formats.add(new CsvContentImporter());
         formats.add(new EndnoteImporter());
         formats.add(new FreeCiteImporter());
         formats.add(new InspecImporter());
